44734a04b0f34c2b5843686045b3d9cd2ad2cb09
dmtr636 <dmtr636@gmail.com>
2022-05-05 17:05:56 +0300

    Some changes

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,10 +1,18 @@
 import {IProduct} from "../../model/IProduct";
 import styles from './Product.module.css'
 import {useState} from "react";
+import {IVariant} from "../../model/IVariant";
 
 function Product(props: {product: IProduct}) {
     const product = props.product
     const [activeVariant, setActiveVariant] = useState(product.variants[0])
+
+    const variantClassNames = (variant: IVariant) => {
+        return styles['variant'] +
+            (variant.name === activeVariant.name
+                ? ' ' + styles['active']
+                : '')
+    }
 
     return (
         <div className={styles.product}>
@@ -17,15 +25,21 @@ function Product(props: {product: IProduct}) {
                     {props.product.portion + " г."}
                 </div>
             </div>
-            <div className={styles['variants']}>
-                {props.product.variants.map(variant =>
-                    <div
-                        className={styles['variant'] + (variant.name === activeVariant.name ? ' ' + styles['active']: '')}
-                        onClick={() => setActiveVariant(variant)}
-                    >
-                        {variant.name}
-                    </div>
-                )}
+            {product.variants.length > 0 &&
+                <div className={styles['variants']}>
+                    {props.product.variants.map(variant =>
+                        <div
+                            className={variantClassNames(variant)}
+                            onClick={() => setActiveVariant(variant)}
+                        >
+                            {variant.name}
+                        </div>
+                    )}
+                </div>
+            }
+            <div className={styles['composition']}>
+                <span className={styles['compositionHeader']}>Состав: </span>
+                <span className={styles['compositionText']}>{activeVariant.composition}</span>
             </div>
         </div>
     )
